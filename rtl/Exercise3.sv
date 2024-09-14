fe4be2ca5fa9e400e35acca5745b426cfd655f25
Dmitri Lyalikov <68623356+DmitriLyalikov@users.noreply.github.com>
2024-09-13 22:28:16 -0400

    feat: complete ex3

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -1,3 +1,4 @@
+
 module Exercise3 (
     input clk,
     input nReset,
@@ -6,5 +7,11 @@ module Exercise3 (
     input [15:0] c,
     output [15:0] out
 );
+
+        wire [7:0] a_in, b_in;
+
+        Mystery1(a[1:0], b[7:0], c[7:0], a_in);   // Alpha
+        Mystery1(a[3:2], b[15:8], c[15:8], b_in); // Beta
+        Mystery2(a_in, b_in, clk, nReset, out);   // Gamma
 
 endmodule
